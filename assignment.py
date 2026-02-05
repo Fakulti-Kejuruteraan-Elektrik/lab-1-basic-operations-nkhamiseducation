5dea3261949e1193912a391184610d0246b06c89
Nurulaqilla Khamis <nurulaqilla@utm.my>
2026-02-06 00:04:05 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,11 +1,9 @@
-from assignment import add_numbers, multiply_numbers
+
 
 def add_numbers(a, b):
     # TODO: Return the sum of a and b
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
+    return a+b
 
 def multiply_numbers(a, b):
     # TODO: Return the product of a and b
-    assert multiply_numbers(3, 4) == 12
-    assert multiply_numbers(10, 0) == 0
+    return a*b
